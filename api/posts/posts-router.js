01b0665e0b3d1becd044895d284d78384590f1b7
Marlon Colato <mcolato2104@gmail.com>
2024-01-14 17:48:40 -0500

    g

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -70,22 +70,24 @@ router.put('/api/posts/:id', (req, res) => {
 
 
 
-router.delete('/api/posts/:id', (req, res) => {
-    const { id } = req.params;
+router.delete('/api/posts/:id', async (req, res) => {
+   try {
+    const deletedPost = await Posts.remove(req.params.id)
 
-    if (id) {
-        Posts.remove(id).then(({ id }) => {
-            return Posts.findById(id);
-        }).then(deletedPost => {
-            res.status(200).json(deletedPost)
-        }).catch(err => {
-            res.status(500).json({ message: "The post could not be removed" })
-        })
-    } else {
+    if(!post){
         res.status(404).json({ message: "The post with the specified ID does not exist" });
     }
+
+
+   } catch(err) {
+    res.status(500).json({ message: "The post could not be removed" })
+   }
+        
+    
 })
 
+ 
+    
 
 
 module.exports = router
